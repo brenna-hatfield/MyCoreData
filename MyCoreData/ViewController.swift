db68953aac3cc4c5e882813413a16272629c9d78
Brenna Cardona <cis290@PC22758.local>
2017-10-12 18:11:39 -0500

    BH

---
+++ MyCoreData/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  MyCoreData
+//
+//  Created by cis290 on 10/12/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var titleseries: UITextField!
+    @IBOutlet weak var author: UITextField!
+    @IBOutlet weak var genre: UITextField!
+    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var btnEdit: UIButton!
+    @IBAction func btnSave(_ sender: UIButton) {
+    }
+    @IBAction func btnEdit(_ sender: UIButton) {
+    }
+    @IBAction func btnBack(_ sender: Any) {
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
